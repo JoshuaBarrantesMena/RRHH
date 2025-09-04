602a53afe2f64dae38793321ec5870031e765b78
lolkingjra <ja2159586@gmail.com>
2025-09-04 15:56:41 -0600

    politica incentivos

--- HumanResources/src/modelo/utils/IncentivoPorProductividad.java
+++ HumanResources/src/modelo/utils/IncentivoPorProductividad.java
@@ -1,9 +1,25 @@
 package modelo.utils;
-import modelo.inter.*;
+import modelo.inter.Incentivo;
 
 public class IncentivoPorProductividad implements Incentivo {
+    private final int umbralHoras;
+    private final double montoBono;
 
-    public double calcularIncentivo(Empleado e) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public IncentivoPorProductividad(int umbralHoras, double montoBono) {
+        this.umbralHoras = umbralHoras;
+        this.montoBono = montoBono;
+    }
+    @Override
+    public double calcularIncentivo(Empleado empleado) {
+        if (empleado instanceof PorHoras trabajadorPorHoras) {
+          if (trabajadorPorHoras.getHorasQuincena() > this.umbralHoras) {
+                return this.montoBono;
+            }
+        }
+    
+        return 0.0;
     }
 }
+    
+    
+
--- HumanResources/src/modelo/utils/PorHoras.java
+++ HumanResources/src/modelo/utils/PorHoras.java
@@ -1,20 +1,33 @@
 package modelo.utils;
+import java.time.LocalDate;
 import modelo.inter.Bonificable;
-
+import modelo.inter.Incentivo;
 public class PorHoras extends Empleado implements Bonificable {
 
     private double tarifaHoras;
 
     private int horasQuincena;
+    private Incentivo politicaDeIncentivo; 
+    public PorHoras(double tarifaHoras, int horasQuincena, String cedula, String nombre, LocalDate fechaIngreso) {
+        super(cedula, nombre, fechaIngreso);
+        this.tarifaHoras = tarifaHoras;
+        this.horasQuincena = horasQuincena;
+        this.politicaDeIncentivo = new IncentivoPorProductividad(80, 20000.0);
 
+    }
     @Override
     public double bono() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return 0.0;
     }
 
     @Override
     public double salarioQuincena() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int topeMaxHoras = 10;
+        if (topeMaxHoras >10){
+        throw new IllegalArgumentException("Tope máximo de horas trabajadas"); }
+        return tarifaHoras*horasQuincena;
     }
-
+public int getHorasQuincena() {
+    return this.horasQuincena; 
+}
 }
